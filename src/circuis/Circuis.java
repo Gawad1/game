f1d86a6f12bd723a137f9b2b26b7fe65cd301325
yousseffayed139 <yousseffayed139@gmail.com>
2022-12-24 23:30:48 +0200

    kbv

--- src/circuis/Circuis.java
+++ src/circuis/Circuis.java
@@ -14,6 +14,7 @@ public class Circuis {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("jhgug");
         // TODO code application logic here
     }
     
