11fd69717be40a0104f7b3ea17cee3084b997bb2
yousseffayed139 <yousseffayed139@gmail.com>
2022-12-26 00:49:39 +0200

    jar

--- src/circuis/Circuis.java
+++ src/circuis/Circuis.java
@@ -1,21 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package circuis;
 
 /**
  *
- * @author pc
+ * @author ADMIN
  */
 public class Circuis {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.print("jhgug777778");
-        // TODO code application logic here
-    }
     
 }
