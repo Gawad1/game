f7acc98b81e92a9da48560481cf05d729b634c56
mohamedsoffar <mohamed.soffar@outlook.com>
2022-12-26 09:20:41 +0200

    momo

--- src/Controller/States.java
+++ src/Controller/States.java
@@ -1,13 +1,37 @@
+package Controller;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controller;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
-/**
- *
- * @author ayahsoffar
- */
+
 public class States {
-    
+    String state="Progress";
+    public String getStates(int score,int time)throws FileNotFoundException, IOException{
+    if(time>0)
+        return state;
+    else {
+    int HS;
+    File file = new File("/hs.txt");
+    Scanner sc = new Scanner(file);
+    HS=Integer.parseInt(sc.nextLine());
+    if(score>HS){
+       HS=score;
+       state="HighScore";
+    }
+    else{
+        state="GameOver";
+    }
+    FileWriter fr = new FileWriter("hello.txt");
+    BufferedWriter br = new BufferedWriter(fr);
+    br.write(HS);
+    return state;
+    }
 }
